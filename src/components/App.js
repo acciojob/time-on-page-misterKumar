1899f4a8ccfe38292e1b96d6ec91a1e8a03e471b
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-12 22:34:11 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import TimeTracker from "./TimeTracker";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <TimeTracker />
     </div>
   )
 }
---
+++ src/components/TimeTracker.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+const TimeTracker = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>You've been on this page for {seconds} seconds.</p>
+    </div>
+  );
+};
+
+export default TimeTracker;
